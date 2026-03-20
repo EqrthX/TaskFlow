ee35f4b6d190e303877ef5a6622095d0dca5d619
nontprawitch <nontprawitch.saetang@gmail.com>
2026-03-20 16:31:21 +0700

    fix: update CORS configuration to allow multiple origins

--- server/src/app.ts
+++ server/src/app.ts
@@ -12,11 +12,20 @@ import userRoute from "./routes/userRotes";
 import taskRoute from "./routes/taskRoutes";
 
 const app = express();
-
+const allowedOrigins = [
+    'http://localhost:5173',
+    'http://74.225.200.27'
+]
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true
-}))
+}));
 app.use(express.json())
 app.use(cookieParser()); // 3. เปิดใช้งาน
 app.use(
