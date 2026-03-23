c157e88515708dc817f3eed6edf9bed31884d2df
nontprawitch <nontprawitch.saetang@gmail.com>
2026-03-23 15:41:27 +0700

    feat: add redis

--- server/src/controllers/taskController.ts
+++ server/src/controllers/taskController.ts
@@ -4,41 +4,45 @@ import { AuthRequest } from "../middlewares/authMiddleware";
 import { AddTaskServices, DeleteTaskServices, UpdateTaskServices, UpdateTaskStatusServices } from "../services/taskServices";
 import prisma from "../config/db";
 import { uploadImageToAzure } from "../services/azureStorage";
-
+import redisClient from "../config/redis";
 
 export const AddTasks = async (req: AuthRequest, res: Response) => {
     logger.info(`Start Controller Registination ${new Date().toDateString()}`);
-    const {title, description, date} = req.body;
+    const { title, description, date } = req.body;
     try {
         const userId = req.user.userId;
         const files = req.files as Express.Multer.File[];
-        const attachmentData: {url: string; fileName: string}[] = [];
+        const attachmentData: { url: string; fileName: string }[] = [];
 
-        if(files && files.length > 0) {
-            for(const file of files) {
+        if (files && files.length > 0) {
+            for (const file of files) {
                 const imageUrl = await uploadImageToAzure(file);
-                attachmentData.push({url: imageUrl, fileName: file.originalname});
+                attachmentData.push({ url: imageUrl, fileName: file.originalname });
             }
         }
-        if(!title || !date || !description) {
-            return res.status(400).json({error: "กรุณาส่งข้อมูล title และ date ให้ครบ"})
+        if (!title || !date || !description) {
+            return res.status(400).json({ error: "กรุณาส่งข้อมูล title และ date ให้ครบ" })
         }
 
         const newTask = await AddTaskServices({
-            title, 
-            description, 
-            date: new Date(date), 
+            title,
+            description,
+            date: new Date(date),
             userId,
             attachments: attachmentData
         });
-
+        const cacheKey = `tasks:${userId}`;
+        if (redisClient.isReady) {
+            await redisClient.del(cacheKey);
+            console.log("🗑️ ล้างแคช Redis เรียบร้อย");
+        }
         return res.status(201).json({
             message: "เพิ่มงานสำเร็จ",
             data: newTask
         })
     } catch (error: unknown) {
-        
-        if(error instanceof Error) {
+
+        if (error instanceof Error) {
             if (error.message === "TASKS_NOTFOUND") {
                 logger.error(`Error Controller AddTasks Email:${error.message} ${new Date().toDateString()}`)
                 return res.status(400).json({ error: "ไม่มีข้อมูลงานที่กรอกเข้ามา" });
@@ -55,8 +59,21 @@ export const AddTasks = async (req: AuthRequest, res: Response) => {
 
 export const showTasks = async (req: AuthRequest, res: Response) => {
     try {
+        const userId = req.user.userId;
+        const cacheKey = `tasks:${userId}`;
+
+        if (redisClient.isReady) {
+            const cachedTasks = await redisClient.get(cacheKey);
+            if (cachedTasks) {
+                console.log("⚡ ดึงข้อมูลจาก Redis Cache");
+                return res.status(200).json({
+                    tasks:JSON.parse(cachedTasks)
+                });
+            }
+        }
+
         const result = await prisma.task.findMany({
-            where:{
+            where: {
                 userId: req.user.userId
             },
             include: {
@@ -66,11 +83,18 @@ export const showTasks = async (req: AuthRequest, res: Response) => {
                 date: 'asc'
             }
         })
+
+        if (redisClient.isReady) {
+            await redisClient.set(cacheKey, JSON.stringify(result), {
+                EX: 3600
+            });
+        }
+
         return res.status(200).json({
             tasks: result
         })
     } catch (error: unknown) {
-        if(error instanceof Error) {
+        if (error instanceof Error) {
             res.status(500).json({
                 error: "Something went wrong to AddTasks",
                 detail: error.message
@@ -81,20 +105,26 @@ export const showTasks = async (req: AuthRequest, res: Response) => {
 
 export const UpdateStatusTask = async (req: AuthRequest, res: Response) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
 
-        const {isDone} = req.body;
+        const { isDone } = req.body;
 
         const userId = req.user.userId;
 
         const updateTask = await UpdateTaskStatusServices(Number(id), isDone, userId);
+
+        const cacheKey = `tasks:${userId}`;
+        if (redisClient.isReady) {
+            await redisClient.del(cacheKey);
+            console.log("🗑️ ล้างแคช Redis เรียบร้อย");
+        }
 
         return res.status(200).json({
             message: "อัพเดตสถานะงานเสร็จสิ้น",
             data: updateTask
         })
     } catch (error: unknown) {
-        if(error instanceof Error) return res.status(400).json({ error: error.message });
+        if (error instanceof Error) return res.status(400).json({ error: error.message });
     }
 }
 
@@ -102,20 +132,26 @@ export const UpdateTask = async (req: AuthRequest, res: Response) => {
     try {
         const userId = req.user.userId;
 
-        const {id} = req.params;
+        const { id } = req.params;
 
-        const {title, description} = req.body;
+        const { title, description } = req.body;
 
         const newUpdateTask = await UpdateTaskServices(Number(id), title, description, userId);
 
+        const cacheKey = `tasks:${userId}`;
+        if (redisClient.isReady) {
+            await redisClient.del(cacheKey);
+            console.log("🗑️ ล้างแคช Redis เรียบร้อย");
+        }
+        
         return res.status(200).json({
-            message:"อัพเดตข้อมูลเสร็จสิ้น",
+            message: "อัพเดตข้อมูลเสร็จสิ้น",
             newUpdateTask
         })
     } catch (error: unknown) {
         let errorMessage = "เกิดข้อผิดพลาดในการอัพเดท";
 
-        if(error instanceof Error){
+        if (error instanceof Error) {
             errorMessage = error.message
         }
 
@@ -125,24 +161,24 @@ export const UpdateTask = async (req: AuthRequest, res: Response) => {
     }
 }
 
-export const DeleteTask = async (req: AuthRequest, res:Response) => {
+export const DeleteTask = async (req: AuthRequest, res: Response) => {
     try {
-        const {id} = req.params;
+        const { id } = req.params;
 
         const userId = req.user.userId;
 
         const deleteTask = await DeleteTaskServices(Number(id), userId);
 
         return res.status(200).json({
-            message:"ลบงานเสร็จสิ้น",
+            message: "ลบงานเสร็จสิ้น",
             data: deleteTask
         })
-        
+
     } catch (error: unknown) {
-        if(error instanceof Error)
-        return res.status(400).json({
-            message: error.message
-        })
+        if (error instanceof Error)
+            return res.status(400).json({
+                message: error.message
+            })
     }
 }
 
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,16 @@
 import app from "./app";
 import logger from "./config/logger";
+import {connectRedis} from "../src/config/redis";
 
 const PORT = process.env.PORT || 3231;
+try {
+    await connectRedis();
 
-app.listen(PORT, () => {
-    logger.info(`Server is running at http://localhost:${PORT}`);
-})
+    app.listen(PORT, () => {
+        logger.info(`Server is running at http://localhost:${PORT}`);
+    })
+    
+} catch (error) {
+    logger.error("Error Running Server:",error)
+    console.error("Error Running Server:", error);
+}
